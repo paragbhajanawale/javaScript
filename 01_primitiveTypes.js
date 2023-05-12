37362fc8e5a060dbeb2cfd9c910c6f6d457b032f
paragbhajanawale <parag.bhajanawale@gmail.com>
2023-05-12 07:27:22 +0530

    primitive type for js and variables
    primitive type for js and variables

---
+++ 01_primitiveTypes.js
@@ -0,0 +1,65 @@
+// Primitive Type
+// 1. Number
+var age = 31;
+var weight = 78;
+var height = 6.1;
+var speckNumber = 0.50;
+console.log(age, weight, height, speckNumber);
+var typeOfAge = typeof age;
+console.log(typeOfAge);
+
+console.log(typeof speckNumber);
+
+
+console.log("Age:", age);
+console.log("Weight:", weight);
+console.log(height);
+console.log(speckNumber);
+
+// 2. Boolean --> true or false
+var isMarried = true;
+var knowAngular = true; 
+var isSoftwareEngineer = true;
+var isBachelor = false; 
+console.log(isBachelor);
+var typeOfIsMarried = typeof isMarried;
+console.log("Type of isMarried:", typeOfIsMarried);
+
+// 3. String --> Sequence of character enclosed in '' or ""
+var firstName = "Bill";
+var position = 'Microsoft Co-founder'; 
+var typeOfFirstName = typeof firstName;
+var city = '';
+console.log( "Type of city ->", typeof city);
+console.log( "Type of variable -> firstName: ", typeOfFirstName );
+console.log("Type of variable --> position: " , typeof position);
+
+//2. Special Data type
+// 1. Undefined
+var address;
+console.log("Address: ", address);
+console.log("type of variable address is", typeof address);
+
+// 2. null
+var pin = null;
+console.log("Value of variable -> pin: ", pin);
+console.log('Type of variable pin is:', typeof pin);
+
+var name="parag";
+console.log(name);
+console.log(typeof name);
+var Number=9527080809;
+console.log(Number);
+console.log(typeof Number);
+
+var neme=null;
+console.log(neme);
+console.log(typeof neme);
+
+var isMarriedd=true;
+console.log(isMarriedd);
+console.log(typeof isMarriedd);
+
+var firstName="bill";
+console.log("first name :",firstName,"type",typeof firstName);
+
---
+++ 02_variablebasicsAssig.js
@@ -0,0 +1,36 @@
+var rollNumber=123;
+console.log("   *** data Type Number***");
+console.log("Roll Number is :---> ",rollNumber);
+console.log("Data Type Of Roll Number :--->",typeof rollNumber);
+console.log("");
+
+console.log("   ***Data Type String***");
+var name="parag bhajanawale";
+console.log("full name :---> ",name);
+console.log("data type of name is :---> ",typeof name);
+console.log("");
+
+console.log("   *** Data Type Boolean***");
+var isangularDeveloper=true;
+console.log("isangularDeveloper :---> ",isangularDeveloper);
+console.log("Data Type od IS Angular Devoloper is :--->",typeof isangularDeveloper);
+console.log("");
+
+console.log("***Differance Between NULL and Undefined***");
+console.log("Undefined :--->");
+console.log("-Undefined Means variable is declared but its value is not assigned .");
+console.log("-Type of Undefined as Undefined.");
+console.log("");
+
+console.log("NULL :--->");
+console.log("-NUll Means variable is declared but its value is assigned as Null value.");
+console.log("-Type of Null is Object.");
+console.log("");
+
+var FatherName;
+console.log("FatherName is :-->",FatherName);
+console.log("Type is:-",typeof FatherName);
+
+var MotherName=null;
+console.log("MotherName is :-->",MotherName);
+console.log("type is",typeof MotherName);
