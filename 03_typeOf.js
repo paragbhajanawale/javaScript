6a9678018e27c0c9317db1cdfb168420ae54630d
paragbhajanawale <parag.bhajanawale@gmail.com>
2023-05-19 13:28:36 +0530

    type of js
    type of js

---
+++ 03_typeOf.js
@@ -0,0 +1,27 @@
+console.log("***Before initialize***");
+var BankName;
+console.log("bank Name:-->",BankName);
+console.log("type of Bank name:-->",typeof BankName);
+console.log("");
+
+console.log("***After Initialization***");
+var bankName="SBI Bank";
+console.log("Bank name :--->",bankName);
+console.log("type if variable Bank name is :---> ",typeof bankName);
+console.log("");
+
+var mark="90";
+console.log("mark--->",mark);
+console.log("Type of mark is--->",typeof mark);
+console.log("");
+
+var isWorking;
+console.log("isworking value Before initialization--->",isWorking);
+var isWorking=true;
+console.log("isworking value After initialization--->",isWorking);
+console.log("");
+
+var totalCount="Hundred and Seven";
+console.log("value=",totalCount,"        type = ",typeof totalCount);
+var totalcount=107;
+console.log("value =",totalcount,"                     type  =",typeof totalcount );
